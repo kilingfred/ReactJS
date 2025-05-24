e104fa6c875c70cff9390a310de9b882f824444d
kilingfred <mr.perepolkin@gmail.com>
2025-05-24 17:58:37 +0300

    fixed for build

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Game from "./scenes/Game/Game";
 import ResultScreen from "././scenes/ResultScreen";
 import { Routes, Route } from "react-router-dom";
 import Error from "./components/Error";
+import Login from "./scenes/Login";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Login />} />
       <Route path="/game" element={<Welcome />} />
       <Route path="/game/:difficulty" element={<Game />} />
       <Route path="/results/:difficulty/" element={<ResultScreen />} />
